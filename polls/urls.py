c80ac5ffad59cc49f25a9cfa49c0da79b0889087
Hanwhy <86160798+hanwoooo@users.noreply.github.com>
2023-02-19 08:22:49 +0000

    plus results.html & learn how to use generic  &  What's pk, queryset & how to know occered error

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,26 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
+app_name = 'polls'
+
+urlpatterns = [
   # view 내부의 index를 보여준다.
-  path('', views.index, name='index'),
+  # ex: /polls/
+  #path('', views.index, name='index'),
+  # ex: /polls/5/
+  #path('<int:question_id>/', views.detail, name='detail'),
+  # ex /polls/5/results/
+  #path('<int:question_id>/results/', views.results, name='results'),
+  # ex /polls/5/vote
+  #path('<int:question_id>/vote/', views.vote, name='vote'),
+
+  # pk ==> 객체들을 구분할 수 있는 구분자 즉 question_id를 구분하여 들어가게끔 한다.
+  path('', views.IndexView.as_view(), name='index'),
+
+  path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+  path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+  
+  path('<int:question_id>/vote/', views.vote, name='vote'),
+  
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,100 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-  return HttpResponse("Hello World!!")
+# 밑의 index, results, detail코드를 generic를 사용하면 이렇게 간단하게 만들 수 있다. 
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_question_list'
+
+  # queryset == 모델로부터 전달받은 객체목록
+  def get_queryset(self):
+    """Return the last five published questions."""
+    return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name='polls/detail.html'  
+
+class ResultsView(generic.DetailView):
+  model=Question
+  template_name='polls/results.html'
+
+def vote(request, question_id):
+  #1
+  # return HttpResponse("You're voting on question %s" % question_id)
+  
+  #2
+  question=get_object_or_404(Question, pk=question_id)
+  try:
+    #request.POST는 키로 전송된 자료에 접근할 수 있도록 해주는 사전같은 객체이다.
+    selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    #선택된 설문id를 문자열로 반환하는 역할
+
+    #예외 처리 --> choice가 없어서 키를 못찾는 경우 에러메세지를 띄운 후 다시 설문 폼을 반환하여준다.
+  except(KeyError, Choice.DoesNotExist):
+      return render(request, 'polls/detail.html', {
+        'question':question,
+        'error_message':"You didn't select a choice.",
+      })
+  else:
+    selected_choice.votes +=1
+    selected_choice.save()
+    # POST데이터를 성공적으로 처리한 후에는 항상 Redirect를 해줘야한다. --> 습관을 들이자
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+  
+# generic 사용 X
+#def index(request):
+  #1
+  #return HttpResponse("Hello World!!")
+  
+  #2
+  # question_list는 발행일순으로 정렬하여 5개까지 보여준다.
+  # pub_date ==> 발행일(출판일)
+  # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  # output = ', '.join([q.question_text for q in latest_question_list])
+  # return HttpResponse(output)
+  
+  #3
+  # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  # template = loader.get_template('polls/index.html')
+  # context = {
+  #     'latest_question_list': latest_question_list,
+  # }
+  # return HttpResponse(template.render(context, request))
+  
+  #4
+  #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  #context = {'latest_question_list': latest_question_list}
+  #return render(request, 'polls/index.html', context)
+
+
+#def detail(request, question_id):
+  #1
+  # return HttpResponse("You're looking at question %s." % question_id)
+
+  #2 에러가 났을 때 Http404 사용
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except Question.DoesNotExist:
+  #   raise Http404("Question does not exist")
+    
+  # return render(request, 'polls/detail.html', {'question': question})
+
+  # 3 에러가 났을 때 get_object_or_404를 사용
+  #question = get_object_or_404(Question, pk=question_id)
+  #return render(request, 'polls/detail.html', {'question': question})
+
+#def results(request, question_id):
+  #1
+  # response = "You're looking at the results of question %s."
+  # return HttpResponse(response % question_id)
+
+  #2
+  #question=get_object_or_404(Question, pk=question_id)
+  #return render(request, 'polls/results.html', {'question':question})
