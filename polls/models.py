3760ee3fcd412ee29dc7ce1a029e074d9ad0e27e
Hanwhy <86160798+hanwoooo@users.noreply.github.com>
2023-02-22 14:15:21 +0000

    2.22 test code & plus comment and change code

--- polls/models.py
+++ polls/models.py
@@ -11,11 +11,17 @@ class Question(models.Model):
 
   # 밑의 __str__ 함수를 통해 shell에서 원하는 문구를 볼 수 있다. 
     def __str__(self):
-        return self.question_text
+      return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+      #1
+      #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+      #2
+      now = timezone.now()
+      # timedelta ==> 두 시간사이의 차이를 알기 위해 사용하는 함수
+      # return의 값은 bool값이므로 이렇게 적어도 상관 없다
+      return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -13,12 +14,23 @@ class IndexView(generic.ListView):
 
   # queryset == 모델로부터 전달받은 객체목록
   def get_queryset(self):
-    """Return the last five published questions."""
-    return Question.objects.order_by('-pub_date')[:5]
+    """Return the last five published questions.(not including those set to be published in the future)."""
+    #1 test코드를 작성해서 버그를 확인하기전
+    #return Question.objects.order_by('-pub_date')[:5]
+
+    #2 
+    # __lte ==> less than or equal 즉 timezon.now 보다 pub_date가 작거나 같은 Question을 받는다.
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
   model = Question
   template_name='polls/detail.html'  
+  
+  def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
   model=Question
