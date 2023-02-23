fd745388ff88055a267cce65c3c2a04c3ee24c9d
Hanwhy <86160798+hanwoooo@users.noreply.github.com>
2023-02-23 14:10:40 +0000

    2.23 costomizing webpage & costomizing admin site but not done..

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,45 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+# 2.23 다시 오게 되면 여기 부분 에러코드 분석하고 고칠것!
+# 수십개의 필드가 있을 경우 폼을 fieldset으로 분할하는 것이 좋다. 
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+#1
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+#2
+# field를 재정렬 하여 커스터마이징하기
+# class QuestionAdmin(admin.ModelAdmin):
+#   fields = ['pub_date', 'question_text']
+  
+# admin.site.register(Question, QuestionAdmin)
+
+#3
+# class QuestionAdmin(admin.ModelAdmin):
+#   fieldsets = [
+#     (None, {'fields':['question_text']}),
+#     ('Date information', {'fields': ['pub_date']}),
+#   ]
+# admin.site.register(Question, QuestionAdmin)
+
+#4 #3에 choice 추가
+#4-1 StackedInline을 TabularInlin으로 바꾸기
+# StackedInline 대신에 TabularInline을 사용하면, 관련된 객체는 좀 더 조밀하고 테이블 기반 형식으로 표시된다.
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+  
+class QuestionAdmin(admin.ModelAdmin):
+  # fieldsets = [
+  #   (None, {'fields':['question_text']}),
+  #   ('Date information', {'fields': ['pub_date']}),
+  # ]
+  # inlines = [ChoiceInline]
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+list_filter = ['pub_date']
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,26 +1,39 @@
 from django.db import models
 # 시간을 나타내기 위해서 import함.
 from django.utils import timezone
+from django.contrib import admin
 import datetime
 
 
 # Create your models here.
+#1
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('data published')
+
+#   # 밑의 __str__ 함수를 통해 shell에서 원하는 문구를 볼 수 있다. 
+#     def __str__(self):
+#       return self.question_text
+
+#     def was_published_recently(self):
+#       #1
+#       #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+#       #2
+#       now = timezone.now()
+#       # timedelta ==> 두 시간사이의 차이를 알기 위해 사용하는 함수
+#       # return의 값은 bool값이므로 이렇게 적어도 상관 없다
+#       return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+#2 decorator 사용한 Question class
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('data published')
-
-  # 밑의 __str__ 함수를 통해 shell에서 원하는 문구를 볼 수 있다. 
-    def __str__(self):
-      return self.question_text
-
+    @admin.display(
+      boolean=True,
+      ordering='pub_date',
+      description='Published recently?',
+    )
     def was_published_recently(self):
-      #1
-      #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-      #2
       now = timezone.now()
-      # timedelta ==> 두 시간사이의 차이를 알기 위해 사용하는 함수
-      # return의 값은 bool값이므로 이렇게 적어도 상관 없다
       return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
